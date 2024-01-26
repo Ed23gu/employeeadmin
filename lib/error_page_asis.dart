492be093da80c55cb6cd7403d23345c0c263f39e
Ed23gu <dewyn23@gmail.com>
2024-01-25 23:32:40 -0500

    ok

--- lib/error_page_asis.dart
+++ lib/error_page_asis.dart
@@ -6,7 +6,7 @@ import 'package:flutter/material.dart';
 
 class ErrorPageAsis extends StatelessWidget {
   const ErrorPageAsis();
-//ok
+//oker
   @override
   Widget build(BuildContext context) {
     return ValueListenableBuilder(
